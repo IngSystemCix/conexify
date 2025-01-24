8a0ef218516c8e9c1078f9f32afbbba13fd01ac1
IngSystemCix <juanromerocollazos@gmail.com>
2025-01-24 18:58:46 -0500

    feat: implements validation form register

--- src/main/java/pe/edu/utp/conexify/validation/FormRegisterValidation.java
+++ src/main/java/pe/edu/utp/conexify/validation/FormRegisterValidation.java
@@ -1,7 +1,9 @@
 package pe.edu.utp.conexify.validation;
 
 import jakarta.annotation.PostConstruct;
-import jakarta.faces.view.ViewScoped;
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.faces.application.FacesMessage;
+import jakarta.faces.context.FacesContext;
 import jakarta.inject.Named;
 import lombok.Getter;
 import lombok.Setter;
@@ -11,13 +13,23 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
-import java.util.stream.IntStream;
+import java.util.logging.Logger;
 
 @Getter
 @Setter
 @Named
-@ViewScoped
+@RequestScoped
 public class FormRegisterValidation implements Serializable {
+
+    private static final Logger LOGGER = Logger.getLogger(FormRegisterValidation.class.getName());
+
+    private String name;
+    private String paternalSurname;
+    private String maternalSurname;
+    private String numberPhone;
+    private String email;
+    private String password1;
+    private String password2;
     private Date birthDay;
     private List<Date> validDates;
 
@@ -34,5 +46,10 @@ public class FormRegisterValidation implements Serializable {
         for (long time = thirteenYearsAgo.getTime(); time <= today.getTime(); time += oneDay) {
             validDates.add(new Date(time));
         }
+    }
+
+    public void register() {
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("User registered successfully"));
+        LOGGER.info("Registering user " + name + " " + paternalSurname + " " + maternalSurname + " with email " + email);
     }
 }
